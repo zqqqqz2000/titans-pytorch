a83bad80fe56084d03ea4f0b01ee480d36471b21
lucidrains <lucidrains@gmail.com>
2025-01-08 10:01:38 -0800

    mlp and qkv projections for neural memory module

--- titans_pytorch/titans.py
+++ titans_pytorch/titans.py
@@ -1,6 +1,16 @@
+from __future__ import annotations
+from functools import partial
+
 import torch
-from torch.nn import Module
+from torch import nn
+from torch.nn import Linear, Module
+from torch.func import functional_call, grad
+
 from einops import rearrange
+
+# constants
+
+LinearNoBias = partial(Linear, bias = False)
 
 # functions
 
@@ -12,9 +22,34 @@ def default(v, d):
 
 # classes
 
+def MLP(dim, depth):
+    layers = []
+
+    for i in range(depth):
+        is_first = i == 0
+
+        if not is_first:
+            layers.append(nn.SiLU())
+
+        layers.append(LinearNoBias(dim, dim))
+
+    return nn.Sequential(*layers)
+
 class NeuralMemory(Module):
-    def __init__(self, dim):
+    def __init__(
+        self,
+        dim,
+        model: Module | None = None
+    ):
         super().__init__()
+
+        if not exists(model):
+            model = MLP(dim, depth = 4)
+
+        self.model = model
+
+        self.to_queries = LinearNoBias(dim, dim)
+        self.to_keys_values = LinearNoBias(dim, dim * 2)
 
     def forward(
         self,
