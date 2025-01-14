fe313a4a69a3e09abe96a6c4de574803cf9f7293
lucidrains <lucidrains@gmail.com>
2025-01-14 06:59:26 -0800

    throw in a test

--- tests/test_titans.py
+++ tests/test_titans.py
@@ -13,3 +13,19 @@ def test_titans():
     retrieved = mem(seq)
 
     assert seq.shape == retrieved.shape
+
+def test_titans_attn_memory():
+    from titans_pytorch import MemoryAttention
+
+    mem = NeuralMemory(
+        dim = 384,
+        model = MemoryAttention(
+            dim = 384
+        ),
+        chunk_size = 64,
+    )
+
+    seq = torch.randn(2, 1024, 384)
+    retrieved = mem(seq)
+
+    assert seq.shape == retrieved.shape
