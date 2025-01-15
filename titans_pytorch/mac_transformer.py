f83309ba41926a7e8d6e95f1f55aed2dd383d1f4
Phil Wang <lucidrains@gmail.com>
2025-01-15 05:50:13 -0800

    it is very strange that MAC performed so well when it has no overlapping windows. why is that?

---
+++ titans_pytorch/mac_transformer.py
@@ -0,0 +1,123 @@
+from __future__ import annotations
+import math
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.nn import Module, ModuleList
+
+from einops.layers.torch import Rearrange
+
+# helpers
+
+def exists(v):
+    return v is not None
+
+def default(v, d):
+    return v if exists(v) else d
+
+# feedforward and attention
+
+class GEGLU(Module):
+    def forward(self, x):
+        x, gate = x.chunk(2, dim = -1)
+        return F.silu(gate) * x
+
+def FeedForward(dim, mult = 4):
+    dim_inner = int(dim * mult * 2 / 3)
+
+    return nn.Sequential(
+        nn.RMSNorm(dim),
+        nn.Linear(dim, dim_inner * 2),
+        GEGLU(),
+        nn.Linear(dim_inner, dim)
+    )
+
+class SegmentedAttention(Module):
+    def __init__(
+        self,
+        dim,
+        segment_len,
+        dim_head = 64,
+        heads = 8,
+    ):
+        super().__init__()
+        self.norm = nn.RMSNorm(dim)
+
+        dim_inner = dim_head * heads
+
+        self.to_qkv = nn.Linear(dim, dim_inner * 3, bias = False)
+        self.to_out = nn.Linear(dim_inner, dim, bias = False)
+
+        self.split_heads = Rearrange('b n (h d) -> b h n d', h = heads)
+        self.merge_heads = Rearrange('b h n d -> b n (h d)')
+
+    def forward(self, x):
+        batch, seq_len = x.shape[:2]
+
+        x = self.norm(x)
+
+        q, k, v = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(self.split_heads, (q, k, v))
+
+        out = F.scaled_dot_product_attention(q, k, v, is_causal = True)
+
+        out = self.merge_heads(out)
+
+        return self.to_out(out)
+
+# MAC transformer
+
+class MemoryAsContextTransformer(Module):
+    def __init__(
+        self,
+        *,
+        num_tokens,
+        dim,
+        depth,
+        segment_len,
+        dim_head = 64,
+        heads = 8,
+        ff_mult = 4
+    ):
+        super().__init__()
+
+        self.token_emb = nn.Embedding(num_tokens, dim)
+
+        self.layers = ModuleList([])
+
+        for _ in range(depth):
+            self.layers.append(ModuleList([
+                SegmentedAttention(dim = dim, dim_head = dim_head, heads = heads, segment_len = segment_len),
+                FeedForward(dim = dim, mult = ff_mult)
+            ]))
+
+        self.norm = nn.RMSNorm(dim)
+
+        self.to_logits = nn.Linear(dim, num_tokens, bias = False)
+
+    def forward(self, x):
+
+        x = self.token_emb(x)
+
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+
+        x = self.norm(x)
+
+        return self.to_logits(x)
+
+# main
+
+if __name__ == '__main__':
+    transformer = MemoryAsContextTransformer(
+        num_tokens = 256,
+        dim = 256,
+        depth = 2,
+        segment_len = 128,
+    )
+
+    x = torch.randint(0, 256, (1, 1023))
+
+    logits = transformer(x)
